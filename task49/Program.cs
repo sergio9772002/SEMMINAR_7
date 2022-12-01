d129c47c8a6d14b1e7e2101e254cba3b900c7f02
Sergio <sergio977@rambler.ru>
2022-12-01 10:05:11 +0500

    add task49

---
+++ task49/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+int[,] generate2Darray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+void print2DArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void printNew2DArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+            array[i, j] = array[i, j] * array[i, j];
+            Console.Write(array[i, j] + "\t");
+            }
+            else
+            {
+                printColor(array[i, j] + "\t", ConsoleColor.DarkCyan);
+            }
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = generate2Darray(5, 7, 100);
+print2DArray(array);
+printNew2DArray(array);
